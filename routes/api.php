ec2fd2936565a04dfbc19876b09293f3ae591a73
Promzy004 <promiseedwin242@gmail.com>
2025-07-10 23:24:51 +0100

    feat: buyer favorite route created

--- routes/api.php
+++ routes/api.php
@@ -32,7 +32,10 @@ Route::middleware(['auth:sanctum','role:admin'])->controller(AdminPropertyContro
 
 
 //buyer Routes
-Route::middleware(['auth:sanctum','role:buyer'])->controller(BuyerPropertyController::class)->group(function () {
+Route::middleware(['auth:sanctum', 'role:buyer'])->controller(BuyerPropertyController::class)->group(function () {
     Route::get('/buyer-all-properties', 'buyerAllProperties');
+    Route::get('/favorite', 'getFavoritedProperties');
     Route::post('/favorite/{id}', 'favorite');
 });
+
+
